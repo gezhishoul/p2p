6bbbb17b715afd60d7579d68d610e10b633ca7bf
Ryan <gezhisoul@gmail.com>
2014-11-04 17:05:39 +0800

    update

---
+++ html/investDetail.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>Examples</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link href="css/style.css" rel="stylesheet" />
+    <link href="css/invest.css" rel="stylesheet" />
+</head>
+<body>
+<?php include 'header.php' ?>
+    <div class="loan container">
+        <div class="loanDetail mt10">
+            <div class="loanDetailTitle">
+                <a class="agreement themeCor fr" href="#">借款协议（范本）</a>
+                <h2>投资创业</h2>
+            </div>
+            <div class="loanDetailContent p20">
+
+            </div>
+        </div>
+    </div>
+<?php include 'footer.php' ?>
+
+</body>
+</html>
