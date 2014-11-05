c39b9fa10b4d2c3b493aa1ced873f029a5d79b4a
Ryan <gezhisoul@gmail.com>
2014-11-05 17:25:19 +0800

    update

--- html/investDetail.php
+++ html/investDetail.php
@@ -15,14 +15,133 @@
         <div class="loanDetail mt10">
             <div class="loanDetailTitle">
                 <a class="agreement themeCor fr" href="#">借款协议（范本）</a>
-                <h2>投资创业</h2>
+                <h2>投资创业 <a class="themeCor ml10" href="#">user</a></h2>
             </div>
-            <div class="loanDetailContent p20">
-
+            <div class="loanDetailContent p20 clearFix">
+                <div class="fl loanDetailLeft">
+                    <div class="clearFix mb20">
+                        <dl class="loanInfo">
+                            <dt>标的总额 （元）</dt>
+                            <dd>￥15,000</dd>
+                        </dl>
+                        <dl class="loanInfo">
+                            <dt>年利率</dt>
+                            <dd>13.00%</dd>
+                        </dl>
+                        <dl class="loanInfo">
+                            <dt>还款期限 （月）</dt>
+                            <dd>24</dd>
+                        </dl>
+                    </div>
+                    <div class="clearFix">
+                        <dl class="loanRules fl">
+                            <dt>保障方式</dt>
+                            <dd>本金</dd>
+                        </dl>
+                        <dl class="loanRules fl">
+                            <dt>提前还款费率</dt>
+                            <dd>1.00%</dd>
+                        </dl>
+                        <dl class="loanRules fl">
+                            <dt>还款方式</dt>
+                            <dd>按月还款/等额本息 </dd>
+                        </dl>
+                        <dl class="loanRules fl">
+                            <dt>月还本息（元）</dt>
+                            <dd>1,008.00</dd>
+                        </dl>
+                    </div>
+                    <dl class="loanDate mt30 clearFix">
+                        <dt class="fl">投标进度</dt>
+                        <dd class="fl">
+                            <div class="progress fl">
+                                <div class="progress-bar highCor" style="width:80%"></div>
+                            </div>
+                            <div class="progressValue highCor fl">
+                                80%
+                            </div>
+                            <div class="fl timeLeft">
+                                剩余时间<span class="fs18">6</span>天<span class="fs18">23</span>时<span class="fs18">59</span>分
+                            </div>
+                        </dd>
+                    </dl>
+                </div>
+                <div class="fl toLoan">
+                    <dl class="moneyLeft">
+                        <dt>剩余金额（元）</dt>
+                        <dd>￥7,200</dd>
+                    </dl>
+                    <p class="mt30 text-right"><span class="fl">账户余额&nbsp;&nbsp;&nbsp;&nbsp;0.00元 </span><a class="themeCor" href="">充值</a></p>
+                    <form class="posr" action="">
+                        <div class="hasHint">
+                            <p class="inputHint">
+                                输入金额需为50元的整数倍
+                            </p>
+                            <input class="loan mt10 mb20 hintInput" type="text" />
+                        </div>
+                        <button class="loanSubmit btn btn-primary">投标</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="loanOwner mt30 tabs">
+            <ul class="tabsTitle clearFix">
+                <li class="active">标的详情</li>
+                <li>投标记录</li>
+            </ul>
+            <div class="tabContent">
+                <div class="tabPane">
+                    <div class="customerInfo">
+                        <h2>用户信息</h2>
+                        <ul class="clearFix">
+                            <li>
+                                <span class="mr30">昵称</span>
+                                <span class="customerInfoValue">xxx</span>
+                            </li>
+                            <li>
+                                <span class="mr30">收入范围</span>
+                                <span class="customerInfoValue">5000-10000元</span>
+                            </li>
+                            <li>
+                                <span class="mr30">工作时间</span>
+                                <span class="customerInfoValue">1-3年</span>
+                            </li>
+                            <li>
+                                <span class="mr30">工作城市</span>
+                                <span class="customerInfoValue">上海 崇明</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="tabPane disn">
+                    <table class="table table-larger table-hover">
+                        <thead>
+                            <tr>
+                                <th>序号</th>
+                                <th>投标人</th>
+                                <th>投标金额</th>
+                                <th>投标时间</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>michaelwusm</td>
+                            <td>4000元</td>
+                            <td>2014-11-05 15:04</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>michaelwusm</td>
+                            <td>4000元</td>
+                            <td>2014-11-05 15:04</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
 <?php include 'footer.php' ?>
-
 </body>
 </html>
--- html/js/common.js
+++ html/js/common.js
@@ -10,10 +10,39 @@ seajs.config({
     }
 });
 seajs.use('$',function(){
+    //dropdown
     $(".dropdown").hover(function(){
         $(this).addClass("open");
     },function(){
         $(this).removeClass("open");
     });
-
+    //tabs
+    if($(".tabs").length > 0){
+        $(".tabsTitle li").click(function(){
+            $(this).addClass("active");
+            $(this).siblings("li").removeClass("active");
+            var toOperate = $(".tabPane").eq($(this).index());
+            toOperate.removeClass("disn");
+            toOperate.siblings(".tabPane").addClass("disn");
+        });
+    };
+    //input hint
+    if(".inputHint"){
+        $(".inputHint").click(function(){
+            $(this).hide();
+            $(".hintInput").focus();
+        });
+        $(".hintInput").click(function(){
+            $(".inputHint").trigger("click");
+        });
+        $("body").click(function(){
+            if($(this).closest(".hasHint").length == 0){
+                console.log(111)
+                $(".inputHint").show();
+            }else{
+                console.log(222)
+                $(".inputHint").hide();
+            }
+        })
+    }
 });
