46ef2c93c72c5e57145c4cbd2c5b01822cebbca2
Ryan <gezhisoul@gmail.com>
2014-11-20 16:35:19 +0800

    update

---
+++ html/investment.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>Examples</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link href="css/style.css" rel="stylesheet" />
+    <link href="css/account.css" rel="stylesheet" />
+</head>
+<body>
+<?php include 'header.php' ?>
+<div class="myAccount container mt30 clearFix">
+    <div class="fr myAccountCenter investment">
+        <div class="p20 clearFix orangeBack">
+            <div class="fr summaryRight">
+                <div class="clearFix">
+                    <div class="fl">
+                        <h4>可用资金</h4>
+                        <p>0.00元</p>
+                    </div>
+                    <div class="fl">
+                        <h4>已充值总额</h4>
+                        <p>0.00元</p>
+                    </div>
+                    <div class="fl mt10">
+                        <a class="themeBtn highCor" href="#">充值</a>
+                    </div>
+                </div>
+                <hr class="mt10"/>
+                <div class="clearFix mt10">
+                    <div class="fl">
+                        <h4>冻结资金</h4>
+                        <p>0.00元</p>
+                    </div>
+                    <div class="fl">
+                        <h4>已提现总额</h4>
+                        <p>0.00元</p>
+                    </div>
+                    <div class="fl mt10">
+                        <a class="themeBtn" href="#">提现</a>
+                    </div>
+                </div>
+            </div>
+            <div class="fl summaryLeft">
+                <h2>账户余额</h2>
+                <p class="warningColor num">
+                    <em>0.00</em>元
+                </p>
+            </div>
+        </div>
+
+    </div>
+    <?php include 'accountSide.php' ?>
+</div>
+<?php include 'footer.php' ?>
+<script type="text/javascript">
+    seajs.use("account");
+</script>
+</body>
+</html>
---
+++ html/investmentTransfer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ryan
+ * Date: 11/20/14
+ * Time: 3:50 PM
+ */ 
